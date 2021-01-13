daaa7c96b76f082eb93aec28bfc941e845d04969
Aadityasiva <67770098+aadityasivaS@users.noreply.github.com>
2021-01-13 10:18:44 +0530

    nothing new

--- components/third-container.jsx
+++ components/third-container.jsx
@@ -8,194 +8,194 @@ export default function ThirdContainer() {
           </h1>
           <div className="row">
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/NextJS.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">NextJS</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/NextJS.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">NextJS</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/React.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">ReactJS</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/React.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">ReactJS</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/Flutter.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">Flutter</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/Flutter.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">Flutter</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/Typescript.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">Typescript</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/Typescript.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">Typescript</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/Javascript.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">Javascript</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/Javascript.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">Javascript</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/html.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">HTML</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/html.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">HTML</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/css.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">CSS</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/css.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">CSS</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/Nodejs.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">NodeJS</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/Nodejs.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">NodeJS</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/Express.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">Express</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/Express.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">Express</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/Bootstrap.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">Bootstrap</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/Bootstrap.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">Bootstrap</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/Bulma.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">Bulma</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/Bulma.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">Bulma</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/Svelte.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">Svelte</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/Svelte.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">Svelte</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/Sapper.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">Sapper</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/Sapper.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">Sapper</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/Unity.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">Unity</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/Unity.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">Unity</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/Unreal.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">Unreal</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/Unreal.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">Unreal</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/Arduino.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">Arduino</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/Arduino.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">Arduino</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/NPM.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">NPM</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/NPM.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">NPM</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/GIT.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">GIT</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/GIT.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">GIT</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/Firebase.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">Firebase</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/Firebase.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">Firebase</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/MongoDB.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">MongoDB</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/MongoDB.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">MongoDB</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/Electron.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">Electron</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/Electron.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">Electron</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/C.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">C</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/C.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">C</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/Java.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">Java</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/Java.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">Java</h5>
                 </div>
               </div>
             </div>
             <div className="col d-flex justify-content-center m-2">
-              <div class="card" style={{width: "13.5rem"}}>
-                <img src="images/MySQL.png" class="card-img-top" alt="..." />
-                <div class="card-body">
-                  <h5 class="card-title fs-1">MySQL</h5>
+              <div className="card" style={{width: "13.5rem"}}>
+                <img src="images/MySQL.png" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title fs-1">MySQL</h5>
                 </div>
               </div>
             </div>
