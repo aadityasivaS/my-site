fdd8b4dab0f7902928a3128eb9239b1e5d0e6786
Aadityasiva <67770098+aadityasivaS@users.noreply.github.com>
2021-01-15 12:57:07 +0530

    Added clarity

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,11 @@ import FifthContainer from '../components/fifth-container';
 export default function Home() {
   useEffect(() => {
     import('bootstrap/dist/js/bootstrap.esm');
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "4zhcqripq9");
   })
   return (
     <div>
